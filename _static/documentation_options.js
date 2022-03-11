5249b041800085c33c6fc6a1a42b7f328a3e5ba5
benmaddison <benm@workonline.africa>
2022-03-11 05:19:20 +0000

    Deploying to gh-pages from @ benmaddison/sphinx-xml2rfc@5c737d70c50deb6af58cfe5049287a1fab06d907 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev62',
+    VERSION: '0.1.1.dev63',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
